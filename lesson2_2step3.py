a797e803ad2b0ef0f24ab4d85e4e2a8f8a27b2d8
unknown <ziravin@yandex.ru>
2022-08-05 01:04:08 +0500

    Корректировка README.md + файл с кодом задания по выпадающим спискам

---
+++ lesson2_2step3.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time 
+
+try:
+    link = "http://suninjuly.github.io/selects2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.ID, "num1")
+    x = x_element.text
+   
+    y_element = browser.find_element(By.ID, "num2")
+    y = y_element.text
+
+    z = str(int(x) + int(y))
+
+    select = Select(browser.find_element(By.TAG_NAME, "select"))
+    select.select_by_value(z)
+
+    
+    button_2 = browser.find_element(By.CLASS_NAME, "btn")
+    button_2.click()
+
+    time.sleep(1)
+
+finally:
+    
+    time.sleep(10)
+    browser.quit()
+
